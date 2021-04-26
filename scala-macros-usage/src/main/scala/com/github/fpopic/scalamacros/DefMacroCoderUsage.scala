0325e506982e211553d7c350cda734337b912bb1
fpopic <filip.popic@gmail.com>
2021-04-26 09:30:59 +0200

    All done except implicit search for Coder[F].

--- scala-macros-usage/src/main/scala/com/github/fpopic/scalamacros/DefMacroCoderUsage.scala
+++ scala-macros-usage/src/main/scala/com/github/fpopic/scalamacros/DefMacroCoderUsage.scala
@@ -1,20 +1,22 @@
 package com.github.fpopic.scalamacros
 
-import org.apache.beam.sdk.coders.Coder
-
-import java.io.{ByteArrayInputStream, ByteArrayOutputStream}
-
-case class Pojo(i: Int, s: Option[Int], l: List[Int])
+import com.github.fpopic.scalamacros.beam.DefMacroCoder
 
 object DefMacroCoderUsage {
+
+  import org.apache.beam.sdk.coders.Coder
+  import java.io.{ByteArrayInputStream, ByteArrayOutputStream}
 
   def main(args: Array[String]): Unit = {
 
     import com.github.fpopic.scalamacros.beam.DefMacroCoder._
+    import com.github.fpopic.scalamacros.beam.DefMacroCoder.{stringCoder, intCoder, listCoder}
+    case class Pojo(s: String, i: Int, l: List[Int])
+    // TODO try with nested class
 
-    val coder: Coder[Pojo] = implicitly
+    val coder: Coder[Pojo] = DefMacroCoder.of[Pojo]
 
-    val pojo = Pojo(1, Some(2), List(3, 4))
+    val pojo = Pojo("4", 1, List(2, 3))
 
     val encoded = {
       val os = new ByteArrayOutputStream()
--- scala-macros/src/main/scala/com/github/fpopic/scalamacros/beam/DefMacroCoder.scala
+++ scala-macros/src/main/scala/com/github/fpopic/scalamacros/beam/DefMacroCoder.scala
@@ -1,31 +1,121 @@
 package com.github.fpopic.scalamacros.beam
 
-import org.apache.beam.sdk.coders.Coder
+import com.github.fpopic.scalamacros.MacrosHelper
+import org.apache.beam.sdk.coders.{Coder, ListCoder, StringUtf8Coder, VarIntCoder}
 
-trait DefMacroCoder[T] extends Coder[T]
+import java.io.{InputStream, OutputStream}
+import java.util
+import java.util.Collections
+import scala.jdk.CollectionConverters._
 
 object DefMacroCoder extends LowPriorityMacros {
 
-  def of[T](implicit c: DefMacroCoder[T]): DefMacroCoder[T] = c
+  def of[T](implicit c: Coder[T]): Coder[T] = c
 
-  // TODO Here add HighPriorityMacros
+  // HighPriorityMacros
 
-  implicit def productCoder[P <: Product]: DefMacroCoder[P] = macro materializeProductCoder[P]
+  implicit val stringCoder: Coder[String] = StringUtf8Coder.of()
+
+  implicit val intCoder: Coder[Int] = new Coder[Int] {
+
+    private val baseCoder = VarIntCoder.of()
+
+    override def encode(i: Int, os: OutputStream): Unit = baseCoder.encode(i, os)
+
+    override def decode(is: InputStream): Int = baseCoder.decode(is)
+
+    override def getCoderArguments: util.List[_ <: Coder[_]] = Collections.emptyList()
+
+    override def verifyDeterministic(): Unit = baseCoder.verifyDeterministic()
+  }
+
+  implicit def listCoder[T](implicit c: Coder[T]): Coder[List[T]] = new Coder[List[T]] {
+
+    private val baseCoder = ListCoder.of(c)
+
+    override def encode(l: List[T], os: OutputStream): Unit = baseCoder.encode(l.asJava, os)
+
+    override def decode(is: InputStream): List[T] = baseCoder.decode(is).asScala.toList // fix it
+
+    override def getCoderArguments: util.List[_ <: Coder[_]] = Collections.emptyList()
+
+    override def verifyDeterministic(): Unit = baseCoder.verifyDeterministic()
+  }
+
+  // invoke macro
+  implicit def productCoder[P <: Product]: Coder[P] = macro materializeProductCoder[P]
 }
 
+//noinspection RedundantBlock
 trait LowPriorityMacros {
 
   import scala.reflect.macros.blackbox
 
-  def materializeProductCoder[T: c.WeakTypeTag](c: blackbox.Context): c.Expr[DefMacroCoder[T]] = {
+  def materializeProductCoder[T: c.WeakTypeTag](c: blackbox.Context): c.Expr[Coder[T]] = {
     import c.universe._
     val tpe = weakTypeOf[T]
+
     println(s"~> Building DefMacroCoder[${tpe}] ...")
 
-    // TODO do the macro magic
+    val helper = new MacrosHelper[c.type](c)
 
-    val ret = q""
-    c.Expr[DefMacroCoder[T]](ret)
+    // For each constructor field generate tree that repr. tuple ("name" -> value)
+    val expressions: Seq[(c.Tree, c.Tree, c.Tree)] =
+      helper.getPrimaryConstructorMembers(tpe).map { field =>
+        val fieldType = field.typeSignature
+        val fieldTerm = field.asTerm
+
+        val fieldEncodeExpression =
+          q"_root_.scala.Predef.implicitly[${fieldType}].encode(value.${fieldTerm}, os)"
+
+        val fieldDecodeExpression =
+          q"${field.asTerm} = _root_.scala.Predef.implicitly[Coder[${fieldType}]].decode(is)"
+
+        val innerType = field.typeSignature.finalResultType // inneryType is String
+        val typeClassType: Type = typeOf(typeTag[Coder[T]]) // typeClassType is Coder[T]
+        val appliedFieldType = appliedType(typeClassType, innerType) // we need Coder[String]
+
+        val foundFieldImplicitCoder = c.inferImplicitValue(appliedFieldType)
+        val fieldImplicitCoderExpression =
+          if (foundFieldImplicitCoder.isEmpty) {
+            c.abort(c.enclosingPosition, s"Implicit search failed for ${appliedFieldType}")
+          } else {
+            q"val ${TermName(c.freshName())}: ${innerType} = ${foundFieldImplicitCoder}"
+          }
+        (fieldEncodeExpression, fieldDecodeExpression, fieldImplicitCoderExpression)
+      }
+
+    val implicitExpressions = expressions.map(_._3).distinct
+    val coderEncodeExpresions = expressions.map(_._1)
+    val coderDecodeExpresions = expressions.map(_._2)
+
+    val classTypeName = TypeName(c.freshName())
+    val coderExpression =
+      q"""class ${classTypeName} extends org.apache.beam.sdk.coders.Coder[${tpe}] {
+
+            ..${implicitExpressions}
+            
+            override def encode(value: ${tpe}, os: java.io.OutputStream): Unit = {
+              ..${coderEncodeExpresions}
+            }
+            
+            override def decode(is: java.io.InputStream): ${tpe} = {
+              ${tpe.typeConstructor}(
+                ..${coderDecodeExpresions}
+              )
+            }
+
+            override def getCoderArguments: java.util.List[_ <: org.apache.beam.sdk.coders.Coder[_]] = java.util.Collections.emptyList
+
+            override def verifyDeterministic(): Unit = ()
+          }
+
+          new ${classTypeName}
+      """
+
+    val ret = coderExpression
+    println(ret)
+    c.Expr[Coder[T]](ret)
   }
 
 }
